d8c267598d36099148d62a5cef21dea7ba08500d
GuiQRSM <endbeob15@gmail.com>
2022-02-28 15:29:52 -0300

    Implements in the 'lib' folder a new dart file called 'Main_Frame' with the 'material.dart' and 'sqflite.dart' imports already established in addition to the 'stful' structure called 'MainDN' returning the 'Scaffold()' class as well is established.

---
+++ lib/Main_Frame.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+
+class MainDN extends StatefulWidget {
+  const MainDN({Key? key}) : super(key: key);
+
+  @override
+  _MainDNState createState() => _MainDNState();
+}
+
+class _MainDNState extends State<MainDN> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
