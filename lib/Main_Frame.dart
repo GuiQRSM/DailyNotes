cf387fc23a36029cb6cfe263df4486ca76f27bee
GuiQRSM <endbeob15@gmail.com>
2022-02-28 15:52:44 -0300

    In the 'Scaffold( )' class, which is returned by the 'stful' structure, it calls the 'backgroundColor:,' attribute receiving the name of the 'secondaryColor' variable.

--- lib/Main_Frame.dart
+++ lib/Main_Frame.dart
@@ -10,14 +10,21 @@ class MainDN extends StatefulWidget {
 
 class _MainDNState extends State<MainDN> {
 
-  var mainColor = Color.fromRGBO(255, 127, 80, 1);
+  var mainColor = Color.fromRGBO(220, 20, 60, 1);
   var secondaryColor =  Color.fromRGBO(240, 128, 128, 1);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: secondaryColor,
       appBar: AppBar(
-
+        title: Text("Daily Notes"),
+        backgroundColor: mainColor,
+        titleTextStyle: TextStyle(
+          fontSize: 19,
+          fontWeight: FontWeight.w600,
+          color: secondaryColor,
+        ),
       ),
     );
   }
